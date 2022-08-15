89da03d4953a568b368e63338b388456531b5caa
BelhardTraining <an.maevskaya@gmail.com>
2022-08-15 21:41:49 +0300

    added comments

--- src/main/java/org/belhard/Main.java
+++ src/main/java/org/belhard/Main.java
@@ -3,6 +3,8 @@ package org.belhard;
 import org.openqa.selenium.WebDriver;
 
 public class Main {
+
+    //This is my program entry method
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
